9c637ccc05b46e4e0005b852d6318ef0da6aee2f
000PaRaDoX000 <ariel.calzada@gmail.com>
2017-04-24 22:54:31 -0500

    Added error pages

--- dead_common/views/tests/basic/normal.py
+++ dead_common/views/tests/basic/normal.py
@@ -2,6 +2,9 @@
 
 from django.views.generic import TemplateView
 from django.contrib import messages
+from django.core.exceptions import SuspiciousOperation # 400
+from django.core.exceptions import PermissionDenied # 403
+from django.http import Http404 # 404
 
 
 class TestsBasicNormalCBV(TemplateView):
@@ -38,6 +41,20 @@ class TestsBasicNormalCBV(TemplateView):
             'Mensaje ERROR'
         )
 
+    def display_error_page(self, code):
+        # 400
+        if code == 1:
+            raise SuspiciousOperation
+        # 403
+        elif code == 2:
+            raise PermissionDenied
+        # 404
+        elif code == 3:
+            raise Http404
+        # 500
+        elif code == 4:
+            raise Exception
+
     def get_context_data(self, **kwargs):
         context = super(TestsBasicNormalCBV, self).get_context_data(**kwargs)
 
@@ -48,6 +65,9 @@ class TestsBasicNormalCBV(TemplateView):
 
         if context['test_name'] == "mensajes":
             self.generar_mensajes()
+        elif context['test_name'] == "error":
+            context['code'] = int(self.request.GET.get("code"))
+            self.display_error_page(context['code'])
 
         return context
 
